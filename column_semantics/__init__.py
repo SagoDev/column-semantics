5464e4370808f9d0e8b91a4d6b2818f0d2b2af25
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2026-02-02 11:33:55 -0300

    refactor(rules): delete old rules module

--- column_semantics/__init__.py
+++ column_semantics/__init__.py
@@ -1,6 +1,5 @@
 """Column Semantics Module."""
 
 from .validators import *
-from .rules import *
 from .utils import *
 from .core import *
--- column_semantics/rules/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Rules Module"""
-
-from .id_rules import id_rule
-
-__all__ = ["id_rule"]
